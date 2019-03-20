634ae9360c9d972b69d175799179a5ef5d3a4c62
KasuganoAichi <terriergamer@gmail.com>
2019-03-19 21:43:44 -0300

    Add files via upload

---
+++ PMD Random Mission Generator.py
@@ -0,0 +1,46 @@
+"""THIS PROGRAM ONLY USE IS TO GENERATE THE TITLE OF A RANDOM MISSION/QUEST
+   FOR POKEMON MYSTERY DUNGEON STYLE GAMES, IT WILL NOT GENERATE ENEMIE
+   POKEMON FOR YOU, IT WILL NOT GENERATE DETAILS OF THE MISSION, IT WILL
+   NOT GENERATE RANDOM ENCOUNTERS"""
+
+"""Prints on screen a list of the available commands
+   for the user"""
+
+def listacomandos():
+    print("Hello and welcome to the random mission generator for Pokemon Mystery Dungeon.")
+    print("Below you can see a list with the available commands,")
+    print("just type the number of the desired command and press Enter to run it")
+
+"""Collect the possible mission names from the mission.txt file"""
+
+def mission():
+
+"""Collect the possible dungeon names from the dungeon.txt file"""
+
+def dungeon():
+
+"""Collect the possible difficulty ranks and rewards from the rewards.txt file"""
+
+def rank():
+
+"""Collect the possible pokemon names from the pokedex.txt file"""
+
+def pokedex():
+
+"""Generate the random mission title only"""
+
+def randomtitlegenerator():
+
+"""Generate a random mission client only"""
+
+def randomclientgenerator():
+
+"""Choose a random difficult rank for the mission"""
+
+def randomrankgenerator():
+
+"""Generate a random reward for the mission"""
+
+def randomrewardgenerator():
+
+"""START OF THE MAIN PROGRAM"""
